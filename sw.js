d5779eba197d9196d1255db9a248fb3564cbc2b8
hibadroit06-oss <hiba.droit06@gmail.com>
2026-04-19 17:30:39 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('fetch', e => {});
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./'));
+});
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SHOW_NOTIF') {
+    self.registration.showNotification(e.data.title, {
+      body: e.data.body,
+      tag: e.data.tag || 'lexup',
+      renotify: true,
+      icon: './icon.svg'
+    });
+  }
+});
